36d3bcd5b5e568447d35419c83fd08a5ae632071
kurozenaku2006 <vedantbhalge2006@gmail.com>
2026-06-07 18:35:49 +0530

    Add claimMethods function with various claim handlers

---
+++ js/claims.js
@@ -0,0 +1,27 @@
+function claimMethods(){
+
+return {
+
+filteredClaims(){ ... },
+
+pendingClaims(){ ... },
+
+approvedClaims(){ ... },
+
+rejectedClaims(){ ... },
+
+revenue(){ ... },
+
+openClaimModal(claim){ ... },
+
+updateClaimStatus(claim,newStatus){ ... },
+
+moveClaimToBin(claim){ ... },
+
+restoreClaim(claim){ ... },
+
+permanentlyDeleteClaim(claim){ ... }
+
+};
+
+}
